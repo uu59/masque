d37e8bfe7f7f103abd008e8d4a914f9226cf9d0f
uu59 <k@uu59.org>
2013-07-20 19:56:02 +0900

    default capybara mode is 1.x

--- lib/masque.rb
+++ lib/masque.rb
@@ -7,7 +7,7 @@ require "masque/version"
 require "masque/dsl"
 
 class Masque
-  attr_reader :options, :agent
+  attr_reader :options, :agent, :compat_mode
 
   def self.run(options = {}, &block)
     new(options).run(&block)
@@ -16,6 +16,18 @@ class Masque
   def initialize(options = {})
     @options = options
     @driver = options[:driver] || :webkit
+
+    case options[:capybara_compat]
+    when "2.1"
+      compat_capybara_21!
+    when "2.0"
+      compat_capybara_20!
+    when "1.x"
+      compat_capybara_1x!
+    else # default compat mode is 1.x (currently. IMO this is most useful for crawler, but any use case can be changed this decision)
+      compat_capybara_1x!
+    end
+
     @agent = Class.new do
       include Masque::DSL
 
@@ -53,7 +65,20 @@ class Masque
     @agent.instance_eval(&block)
   end
 
+  def compat_capybara_21!
+    # https://github.com/jnicklas/capybara/blob/2.1.0/lib/capybara.rb#L323
+    @compat_mode = "2.1"
+    Capybara.configure do |config|
+      config.ignore_hidden_elements = true
+      config.match = :smart
+      config.exact = false
+      config.visible_text_only = false
+    end
+  end
+
   def compat_capybara_20!
+    # http://www.elabs.se/blog/60-introducing-capybara-2-1
+    @compat_mode = "2.0"
     Capybara.configure do |config|
       config.match = :one
       config.exact_options = true
@@ -63,6 +88,8 @@ class Masque
   end
 
   def compat_capybara_1x!
+    # http://www.elabs.se/blog/60-introducing-capybara-2-1
+    @compat_mode = "1.x"
     Capybara.configure do |config|
       config.match = :prefer_exact
       config.ignore_hidden_elements = false
--- spec/masque_spec.rb
+++ spec/masque_spec.rb
@@ -3,11 +3,40 @@
 require "spec_helper"
 
 describe Masque do
-  it ".new" do
-    opts = {:display => 50, :driver => :webkit}
-    masque = Masque.new(opts)
-    masque.options.should == opts
-    masque.respond_to?(:run).should be_true
+  describe ".new" do
+    it do
+      opts = {:display => 50, :driver => :webkit}
+      masque = Masque.new(opts)
+      masque.options.should == opts
+      masque.respond_to?(:run).should be_true
+    end
+
+    describe "compat_mode" do
+      subject { Masque.new(:capybara_compat => ver).compat_mode }
+
+      context "2.1" do
+        let(:method) { :compat_capybara_21! }
+        let(:ver) { "2.1" }
+        it { should == ver }
+      end
+
+      context "2.0" do
+        let(:method) { :compat_capybara_20! }
+        let(:ver) { "2.0" }
+        it { should == ver }
+      end
+
+      context "1.x" do
+        let(:method) { :compat_capybara_1x! }
+        let(:ver) { "1.x" }
+        it { should == ver }
+      end
+    end
+
+    it "default compat mode is 1.x" do
+      Masque.any_instance.should_receive(:compat_capybara_1x!)
+      Masque.new
+    end
   end
 end
 
